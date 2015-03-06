c3e03d181f432ec62ddb364f830cde6145de38af
powatm <powatm@rpi.edu>
2015-03-06 17:00:10 -0500

    Moved validation to separate js file

---
+++ resources/validate.js
@@ -0,0 +1,33 @@
+function validateLogin() {
+        var x = document.forms["login"]["email"].value;
+        if (x == null || x == "") {
+            alert("Email required");
+            return false;
+        }
+        else{ //NEEDS WORK- WON'T REDIRECT
+            alert("success");
+            window.location.href= "http://www.tcktr.tk/tcktr/admin/admin.html";
+            return false;
+        }
+    }
+
+function validateRegister() {
+        var x = document.forms["register"]["user"].value;
+        if (x == null || x == "") {
+            alert("Please select a longer username");
+            return false;
+        }
+        var y = document.forms["register"]["password"].value;
+        var y2 = document.forms["register"]["pass2"].value;
+        if (y == null || y == "" || y.length<8) {
+            alert("Please select a longer password");
+            return false;
+        }
+        if (y!=y2) {
+            alert("Passwords must match");
+            return false;
+        }
+        alert("success");
+         window.location.href= "http://www.tcktr.tk/tcktr/admin/admin.html";
+        return false;
+    }

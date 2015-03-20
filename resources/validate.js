29f86bf7c2ead2144f6cc158b564d8b3b9e92b26
Daniel Ben-Chitrit <dibol13@gmail.com>
2015-03-20 16:52:34 -0400

    Fixed Validation
    Changed HTML and Javascript code to have working validation. Next steps
    are to link it to the backend DB and have actual user validation.

--- resources/validate.js
+++ resources/validate.js
@@ -1,40 +1,34 @@
 function validateLogin() {
-        console.log("Test");
-        var x = document.forms["login"]["email"].value;
-        console.log(x);
-        if (x == null || x == "") {
+        var email = document.forms["login"]["email"].value;
+        var pass = document.forms["login"]["email"].value;
+        if (email == null || email == "") {
             alert("Email required");
             return false;
         }
-        else{ //NEEDS WORK- WON'T REDIRECT
-            alert("success");
-            window.location.href= "http://www.tcktr.tk/tcktr/admin/admin.html";
+        if (pass == null || pass == "") {
+            alert("Password required");
             return false;
+        }
+        else{ 
+            window.location.href= "http://www.tcktr.tk/tcktr/admin/admin.html";
         }
     }
       
-      window.location.href= "http://www.tcktr.tk/tcktr/admin/admin.html";
     // validation was successful
-    return true;
-  }
 
 function validateRegister() {
-        var x = document.forms["register"]["user"].value;
-        if (x == null || x == "") {
+        var user = document.forms["register"]["user"].value;
+        if (user == null || user == "") {
             alert("Please select a longer username");
             return false;
         }
-        var y = document.forms["register"]["password"].value;
-        var y2 = document.forms["register"]["pass2"].value;
-        if (y == null || y == "" || y.length<8) {
+
+        var pass = document.forms["register"]["password"].value;
+        if (pass == null || pass == "" || pass.length<8) {
             alert("Please select a longer password");
             return false;
         }
-        if (y!=y2) {
-            alert("Passwords must match");
-            return false;
-        }
-        alert("success");
-         window.location.href= "http://www.tcktr.tk/tcktr/admin/admin.html";
+
+        window.location.href= "http://www.tcktr.tk/tcktr/admin/admin.html";
         return true;
     }

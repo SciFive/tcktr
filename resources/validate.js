e786c6f4e084370475d92134f03ff0e3db005863
Daniel Ben-Chitrit <dibol13@gmail.com>
2015-04-03 16:23:22 -0400

    Finalized Validation
    Finished all field validation on client side, javascript. Will attach to
    DB once available.

--- resources/validate.js
+++ resources/validate.js
@@ -1,6 +1,6 @@
 function validateLogin() {
         var email = document.forms["login"]["email"].value;
-        var pass = document.forms["login"]["email"].value;
+        var pass = document.forms["login"]["pass"].value;
         if (email == null || email == "") {
             alert("Email required");
             return false;
@@ -19,13 +19,25 @@ function validateLogin() {
 function validateRegister() {
         var user = document.forms["register"]["user"].value;
         if (user == null || user == "") {
-            alert("Please select a longer username");
+            alert("Please select a username");
+            return false;
+        }
+
+        var email = document.forms["register"]["email"].value;
+        if (email == null || email == "") {
+            alert("Please put a valid email address");
             return false;
         }
 
         var pass = document.forms["register"]["password"].value;
         if (pass == null || pass == "" || pass.length<8) {
             alert("Please select a longer password");
+            return false;
+        }
+
+        var check = document.forms["register"]["terms"];
+        if (check.checked == false) {
+            alert("Please check terms and conditions");
             return false;
         }
 

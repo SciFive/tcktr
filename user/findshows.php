27b70fb870dec629a09ec4af4faee1ca49b848f8
powatm <powatm@rpi.edu>
2015-05-07 13:29:09 -0400

    Removed Find Theaters from navbar

--- user/findshows.php
+++ user/findshows.php
@@ -21,7 +21,6 @@
             <a class="pure-menu-heading" href="../index.html">tcktr</a>
             <ul class="pure-menu-list">
                 <li class="pure-menu-item"><a href="./findshows.php" class="pure-menu-link">Shows</a></li>
-                <li class="pure-menu-item"><a href="./findtheaters.html" class="pure-menu-link">Theatres</a></li>
                 <li class="pure-menu-item"><a href="./about.html" class="pure-menu-link">About Us</a></li>
                 <li class="pure-menu-item"><i class="fa fa-shopping-cart fa-2x"></i></li>
             </ul>

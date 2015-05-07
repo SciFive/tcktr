c138330a62153c8e3aa4b5eb4a68020b1c87301a
Daniel Ben-Chitrit <dibol13@gmail.com>
2015-05-07 14:13:23 -0400

    PHP insert doesnt work
    Moving Register back to Validate Javascript

--- resources/insert.php
+++ resources/insert.php
@@ -1,12 +1,13 @@
 <?php
 
-	$db = new PDO("mysql:host=localhost;dbname=tcktr","root","hardlyapassword1!");
+	//$db = new PDO("mysql:host=localhost;dbname=tcktr","root","hardlyapassword1!");
+	$db = new PDO("mysql:host=localhost;dbname=tcktr","root","root");
 
 	$sql = $db->prepare("INSERT INTO admins (username, password, email)
 		VALUES ('$_POST[user]', '$_POST[password]','$_POST[email]')");
 
 	$sql->execute();
 
-	header("../index.html");
+	//header("../index.html");
 
 ?>
